dfc8eec1ce651f35d188f7b669bc7ff8f3b0f6a6
Lucas Mercado <luke.isla@gmail>
2015-06-20 14:21:06 -0700

    Finished the basic draft of machine_spec.rb

--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -5,15 +5,33 @@ require_relative '../model/machine'
 describe Machine do
   before do
     @machine = Machine.new()
+    @hundred = Currency.new("Hundred", 10000, @fifty, @machine)
+    @machine.largest_currency = @hundred
   end
 
   subject {@machine}
 
+  it { is_expected.to respond_to(:change) }
+  it { is_expected.to respond_to(:largest_currency) }
+
   describe "make_change" do
+    it "calls largest_currency.add" do
+      expect(@hundred).to receive(:add)
+      @machine.make_change(100.00)
+    end
+
+    it "calls change.to_s" do
+      expect(@machine.change).to receieve(:to_s)
+      @machine.make_change(100.00)
+    end
   end
 
-  describe "add_root_currency" do
+  # Machine.add_currency functions as a wrapper for change.add_currency to shield Currency
+  # from an unnecessary interdependency.
+  describe "add_currency" do
+    it "calls change.add_currency" do
+      expect(@machine.change).to receieve(:add_currency)
+      @machine.add_currency(Currency.new("Hundred", 10000, @fifty, @machine))
+    end
   end
-
-  #Currency has to know about machine and vice versa, but machine just owns change
 end

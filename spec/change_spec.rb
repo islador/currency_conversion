6ba2dc8e3031339d498860ad1597f41dfe67a4f4
Lucas Mercado <luke.isla@gmail>
2015-06-20 16:16:25 -0700

    Wrote out the first draft of change_spec.rb

---
+++ spec/change_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+require_relative '../model/change'
+require_relative '../model/currency'
+require_relative '../model/machine'
+
+describe Change do
+
+  before do
+    @change = Change.new()
+    @machine = Machine.new()
+  end
+
+  subject {@change}
+
+  it { is_expected.to respond_to(:amounts) }
+
+  describe "add_currency" do
+    it "throws an error if a currency object is not passed" do
+      expect {@change.add("Cash")}.to raise_error(ArgumentError)
+    end
+
+    context "amounts hash does not contain that name" do
+      it "adds the currency's name to the amounts hash" do
+        expect(@change.amounts["Penny"]).to be_nil
+        @change.add_currency(Currency.new("Penny", 1, nil, @machine))
+        expect(@change.amounts["Penny"]).not_to be_nil
+      end
+
+      it "adds a value of one to amounts['currency_name']" do
+        expect(@change.amounts["Penny"]).to be_nil
+        @change.add_currency(Currency.new("Penny", 1, nil, @machine))
+        expect(@change.amounts["Penny"]).to be 1
+      end
+    end
+
+    context "amounts hash contains that name" do
+      it "increments the value of amounts['currency_name']" do
+        expect(@change.amounts["Penny"]).not_to be_nil
+        expect(@change.amounts["Penny"]).to be 1
+        @change.add_currency(Currency.new("Penny", 1, nil, @machine))
+        expect(@change.amounts["Penny"]).to be 2
+      end
+    end
+  end
+
+  describe "to_s" do
+    @change.amounts = {"Hundred" => 2, "Fifty" => 1, "Twenty" => 1}
+    it "returns a formatted string of the amounts hash" do
+      expect(@change.to_s).to match "Change:\n 2 Hundred\n 1 Fifty\n 1 Twenty\n"
+    end
+  end
+end
